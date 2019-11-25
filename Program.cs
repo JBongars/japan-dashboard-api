fce42d77913dcfa980659f1440b5464190be2d7f
Julien Bongars <julien.bongars@axa.com.sg>
2019-11-25 08:37:11 +0800

    chore: removed unused dependencies

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using japan_dashboard_api.Store;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
