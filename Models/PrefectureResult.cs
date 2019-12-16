13501318a2a2b78519081a982e0cfd8dc6915109
Julien Bongars <julien.bongars@axa.com.sg>
2019-12-16 23:25:44 +0800

    chore: renamed prefecture to prefectureDetails

--- Models/PrefectureResult.cs
+++ Models/PrefectureResult.cs
@@ -2,13 +2,13 @@ namespace japan_dashboard_api.Models
 {
     public class PrefectureResult
     {
-        public Prefecture prefecture { get; set; }
+        public Prefecture prefectureDetails { get; set; }
         public long population { get; set; }
 
         public PrefectureResult (Prefecture prefecture, long population)
         {
             this.population = population;
-            this.prefecture = prefecture;
+            this.prefectureDetails = prefecture;
         }
 
     }
